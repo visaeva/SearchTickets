d39b9b74d72fc97e9df8498a3bcae6ec8398e82a
visaeva <visaeva0302@gmail.com>
2024-06-18 12:21:37 +0300

    Initial Commit

---
+++ SearchTickets/SearchTicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTicketsApp.swift
+//  SearchTickets
+//
+//  Created by Victoria Isaeva on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SearchTicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
